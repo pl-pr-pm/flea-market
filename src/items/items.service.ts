d7fe99e2fa9fd119a7a3597ac78ec232bac2157b
pl-pr-pm <pl.pr.pm.12@gmail.com>
2022-02-04 19:09:35 +0900

    CRUD作成

--- src/items/items.service.ts
+++ src/items/items.service.ts
@@ -1,8 +1,35 @@
 import { Injectable } from '@nestjs/common';
+import { ItemStatus } from './item-status.enum';
+import { Item } from './item.model';
 
 @Injectable()
 export class ItemsService {
-  findAll() {
-    return 'This is ItemsService';
+  private items: Item[] = [];
+
+  findAll(): Item[] {
+    return this.items;
+  }
+
+  findById(id: string): Item {
+    return this.items.find((item) => item.id === id);
+    // return this.items.find((item) => {
+    //   console.log('item id', item.id);
+    //   item.id === id;
+    // });
+  }
+
+  create(item: Item): Item {
+    this.items.push(item);
+    return item;
+  }
+
+  updateStatus(id: string): Item {
+    const item = this.findById(id);
+    item.status = ItemStatus.SOLD_OUT;
+    return item;
+  }
+
+  delete(id): void {
+    this.items = this.items.filter((item) => item.id !== id);
   }
 }
