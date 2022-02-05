3017b05710dfd8c60c404c2e4556ba85008bdac5
pl-pr-pm <pl.pr.pm.12@gmail.com>
2022-02-05 13:56:26 +0900

    typeORMの設定

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,21 @@
 import { Module } from '@nestjs/common';
 import { ItemsModule } from './items/items.module';
-
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule } from '@nestjs/config';
 @Module({
-  imports: [ItemsModule],
+  imports: [
+    ItemsModule,
+    ConfigModule.forRoot(),
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: process.env.DB_HOST,
+      port: parseInt(process.env.DB_PORT),
+      username: process.env.DB_USERNAME,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DATABASE,
+      autoLoadEntities: true,
+    }),
+  ],
   controllers: [],
   providers: [],
 })
