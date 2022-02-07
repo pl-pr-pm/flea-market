6c9c21bf45d540da0eb4bfd47c08f8f4394d18a8
pl-pr-pm <pl.pr.pm.12@gmail.com>
2022-02-07 16:03:40 +0900

    マイグレーション実行

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,8 +2,14 @@ import { Module } from '@nestjs/common';
 import { ItemsModule } from './items/items.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { ConfigModule } from '@nestjs/config';
+import { AuthModule } from './auth/auth.module';
 @Module({
-  imports: [ItemsModule, ConfigModule.forRoot(), TypeOrmModule.forRoot()],
+  imports: [
+    ItemsModule,
+    ConfigModule.forRoot(),
+    TypeOrmModule.forRoot(),
+    AuthModule,
+  ],
   controllers: [],
   providers: [],
 })
