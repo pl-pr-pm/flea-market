8c1821cb88d0a4a8ca84868d13bf3927fe4161c8
pl-pr-pm <pl.pr.pm.12@gmail.com>
2022-02-07 18:57:29 +0900

    ログイン機能作成

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,12 +1,19 @@
 import { Body, Controller, Post } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { CreateUserDto } from './dto/create-user.dto';
+import { CredentialsDto } from './dto/credentials.dto';
 
 @Controller('auth')
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
-  @Post()
+  @Post('signup')
   async signUp(@Body() createUserDto: CreateUserDto) {
     return await this.authService.signUp(createUserDto);
+  }
+  @Post('signin')
+  async signIn(
+    @Body() credentialsDto: CredentialsDto,
+  ): Promise<{ accessToken: string }> {
+    return await this.authService.signIn(credentialsDto);
   }
 }
--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -3,9 +3,23 @@ import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
 import { UserRepository } from './auth.repository';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { PassportModule } from '@nestjs/passport';
+import { JwtModule } from '@nestjs/jwt';
+import { JWT_SECRET_KEY, JWT_EXPIRE } from '../env/index';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([UserRepository])],
+  imports: [
+    TypeOrmModule.forFeature([UserRepository]),
+    PassportModule.register({ defaultStrategy: 'jwt' }),
+    JwtModule.register({
+      // secret: process.env.JWT_SECRET_KEY,
+      secret: JWT_SECRET_KEY,
+      signOptions: {
+        // expiresIn: parseInt(process.env.JWT_EXPIRE),
+        expiresIn: JWT_EXPIRE,
+      },
+    }),
+  ],
   controllers: [AuthController],
   providers: [AuthService],
 })
